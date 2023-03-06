2c1dd7e7e312f107631ed82fab2aa62ae02d1bd1
sorenaakjaer <77328754+sorenaakjaer@users.noreply.github.com>
2023-03-06 20:29:40 +0100

    c

--- script.js
+++ script.js
@@ -1809,7 +1809,7 @@ $(document).one("trigger::vue_loaded", function () {
 					return { id: item.id, value: item.value };
 				});
 
-				console.log(formValue)
+				console.log(formValue);
 				$('.INPUT_ONP_EndCustomerEmailConfigFORM > input').val(JSON.stringify(formValue))
 				console.log(JSON.stringify(formValue))
 				$('.BUTTON_ONP_EndCustomerEmailConfigFORM > a').click()
